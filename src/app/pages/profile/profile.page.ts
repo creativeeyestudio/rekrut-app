7cacfd9e402dc783072c89c7d2e75fab89df5404
creativeyefr <contact@creative-eye.fr>
2024-11-15 18:31:53 +0100

    KR - Menu de paramètres

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonGrid, IonCol, IonAvatar, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { GlobalService } from 'src/app/services/global.service';
+
+import { addIcons } from 'ionicons';
+import { logoIonic, settingsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonLabel, IonAvatar, IonCol, IonGrid, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ProfilePage implements OnInit {
 
-  constructor(public globalService: GlobalService) { }
+  constructor(public globalService: GlobalService) {
+    addIcons({ logoIonic, settingsOutline });
+  }
 
   ngOnInit() {
   }
