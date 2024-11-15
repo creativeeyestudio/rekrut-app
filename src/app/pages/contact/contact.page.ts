f47c9c49d1d7f7de65b61a20cad462eca25aa86b
creativeyefr <contact@creative-eye.fr>
2024-11-15 21:29:52 +0100

    Page contact

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { callOutline, phonePortraitOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-contact',
@@ -12,7 +14,9 @@ import { IonicModule } from '@ionic/angular';
 })
 export class ContactPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ callOutline })
+  }
 
   ngOnInit() {
   }
