95405a84917b819c4481b1b5949f83176cd16d15
creativeyefr <contact@creative-eye.fr>
2024-11-15 16:14:28 +0100

    KR - Navigation des formulaires

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonButton, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonButton, IonImg, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonImg, IonButton, IonRow, IonGrid, IonCol, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonInput, IonImg, IonButton, IonRow, IonGrid, IonCol, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class RegisterPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleBlock(blockId: string) {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => {
+      block.classList.add('ion-hide')
+    });
+    document.getElementById(blockId)?.classList.remove('ion-hide');
   }
 
 }
