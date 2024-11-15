39ecda7e1a620a6cb880e18721b42dd14b53f140
creativeyefr <contact@creative-eye.fr>
2024-11-15 19:32:25 +0100

    Icones du bas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
+import { addIcons } from 'ionicons';
+import { flagOutline, heartOutline, informationCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    addIcons({heartOutline, informationCircleOutline, flagOutline})
   }
 
 }
