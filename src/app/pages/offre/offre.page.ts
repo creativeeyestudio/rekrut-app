6530c427aaa9d35af41f9ec33efab953e5492700
creativeyefr <contact@creative-eye.fr>
2024-11-16 05:21:45 +0100

    Fiche de poste

--- src/app/pages/offre/offre.page.ts
+++ src/app/pages/offre/offre.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { chevronBackOutline } from 'ionicons/icons';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-offre',
   templateUrl: './offre.page.html',
   styleUrls: ['./offre.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class OffrePage implements OnInit {
 
-  constructor() { }
+  constructor(public global: GlobalService) {
+    addIcons({ chevronBackOutline })
+  }
 
   ngOnInit() {
   }
