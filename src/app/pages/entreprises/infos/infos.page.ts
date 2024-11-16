5c8fde8dd81177f64e84147ea074aa98eb1ff0ae
creativeyefr <contact@creative-eye.fr>
2024-11-16 04:45:40 +0100

    Fiche d'entreprise

--- src/app/pages/entreprises/infos/infos.page.ts
+++ src/app/pages/entreprises/infos/infos.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { chevronBackOutline, chevronForwardOutline } from 'ionicons/icons';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-infos',
   templateUrl: './infos.page.html',
   styleUrls: ['./infos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class InfosPage implements OnInit {
 
-  constructor() { }
+  constructor(public global: GlobalService) {
+    addIcons({ chevronForwardOutline, chevronBackOutline })
+  }
 
   ngOnInit() {
   }
