4990f8c919e72e8d017443d2c798d5073adfa973
tomas-ryan-lee <rifakev@gmail.com>
2024-12-23 14:11:03 +0100

    Gallery

--- src/app/pages/upload/upload.page.ts
+++ src/app/pages/upload/upload.page.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.page.html',
   styleUrls: ['./upload.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class UploadPage implements OnInit {
+export class UploadPage {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  numbers = Array.from({ length: 9 }, (_, i) => i + 1);
 
 }
