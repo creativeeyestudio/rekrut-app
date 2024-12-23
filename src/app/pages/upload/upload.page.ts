802377a75b0c845ac7bf7e07f7f7e746cb0e0a69
tomas-ryan-lee <rifakev@gmail.com>
2024-12-23 14:30:43 +0100

    Back Btn

--- src/app/pages/upload/upload.page.ts
+++ src/app/pages/upload/upload.page.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+import { chevronBackOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.page.html',
@@ -11,6 +14,10 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class UploadPage {
+
+  constructor() {
+    addIcons({ chevronBackOutline })
+  }
 
   numbers = Array.from({ length: 9 }, (_, i) => i + 1);
 
