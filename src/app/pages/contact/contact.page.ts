5cfb874d9858e1410daa1f44903cbf08e3e6ae7f
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-17 18:25:02 +0100

    Select image

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -15,6 +15,8 @@ import { attachOutline, callOutline, phonePortraitOutline } from 'ionicons/icons
 export class ContactPage {
 
   isModalOpen = false;
+  numbers = Array.from({ length: 9 }, (_, i) => i + 1);
+  selectedImages: string[] = []; // Tableau pour stocker les images sélectionnées
 
   constructor() {
     addIcons({ callOutline, attachOutline })
@@ -23,5 +25,20 @@ export class ContactPage {
   setOpen(isOpen: boolean) {
     console.log(isOpen);
     this.isModalOpen = isOpen;
+  }
+
+  toggleImageSelection(imageUrl: string) {
+    const index = this.selectedImages.indexOf(imageUrl);
+    if (index > -1) {
+      // Si l'image est déjà sélectionnée, on la retire
+      this.selectedImages.splice(index, 1);
+    } else {
+      // Sinon, on l'ajoute
+      this.selectedImages.push(imageUrl);
+    }
+  }
+
+  isSelected(imageUrl: string): boolean {
+    return this.selectedImages.includes(imageUrl);
   }
 }
