f1aa8ac97211737ef6b754e17181ee58bdf31f5a
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-10 03:34:35 +0100

    PDF Modal

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,89 +1,124 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChildren,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, GestureController } from '@ionic/angular';
 
 import { addIcons } from 'ionicons';
-import { flagOutline, heart, heartOutline, informationCircleOutline, sendOutline } from 'ionicons/icons';
+import {
+  flagOutline,
+  heart,
+  heartOutline,
+  informationCircleOutline,
+  sendOutline,
+} from 'ionicons/icons';
 import { GlobalService } from 'src/app/services/global.service';
 
-import { register } from "swiper/element/bundle";
+import { register } from 'swiper/element/bundle';
 register();
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.page.html',
-	styleUrls: ['./home.page.scss'],
-	standalone: true,
-	imports: [IonicModule, CommonModule, FormsModule],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA], 
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
 export class HomePage implements OnInit {
-	@ViewChildren('cardElement') cardElements!: QueryList<ElementRef>;
-	isLiked: boolean = false;
+  @ViewChildren('cardElement') cardElements!: QueryList<ElementRef>;
+  isLiked: boolean = false;
 
-	videos = [
-		{ id: 1, url: 'assets/videos/video1.mp4', isLiked: false },
-		{ id: 2, url: 'assets/videos/video2.mp4', isLiked: false },
-		{ id: 3, url: 'assets/videos/video3.mp4', isLiked: false },
-	];
+  videos = [
+    { id: 1, url: 'assets/videos/video1.mp4', isLiked: false },
+    { id: 2, url: 'assets/videos/video2.mp4', isLiked: false },
+    { id: 3, url: 'assets/videos/video3.mp4', isLiked: false },
+  ];
 
-	alertButtons = ['Contacter', 'Fermer'];
+  alertButtons = ['Contacter', 'Fermer'];
 
+  modals: { [key: string]: boolean } = {};
 
-	constructor(public global: GlobalService, private gestureCtrl: GestureController) { }
+  constructor(
+    public global: GlobalService,
+    private gestureCtrl: GestureController,
+	private cdr: ChangeDetectorRef
+  ) {}
 
-	ngOnInit() {
-		addIcons({ heart, heartOutline, informationCircleOutline, flagOutline, sendOutline });
-	}
+  ngOnInit() {
+    addIcons({
+      heart,
+      heartOutline,
+      informationCircleOutline,
+      flagOutline,
+      sendOutline,
+    });
+  }
 
-	ngAfterViewInit() {
-		this.cardElements.forEach((card, index) => {
-			const gesture = this.gestureCtrl.create({
-				el: card.nativeElement,
-				gestureName: 'swipe',
-				onMove: (ev) => this.onMove(ev, card.nativeElement),
-				onEnd: (ev) => this.onEnd(ev, card.nativeElement, index),
-			});
-			gesture.enable();
-		});
-	}
+  ngAfterViewInit() {
+    this.cardElements.forEach((card, index) => {
+      const gesture = this.gestureCtrl.create({
+        el: card.nativeElement,
+        gestureName: 'swipe',
+        onMove: (ev) => this.onMove(ev, card.nativeElement),
+        onEnd: (ev) => this.onEnd(ev, card.nativeElement, index),
+      });
+      gesture.enable();
+    });
+  }
 
-	onMove(ev: any, card: HTMLElement) {
-		card.style.transform = `translate(${ev.deltaX}px, ${ev.deltaY}px) rotate(${ev.deltaX / 10}deg)`;
-	}
+  onMove(ev: any, card: HTMLElement) {
+    card.style.transform = `translate(${ev.deltaX}px, ${ev.deltaY}px) rotate(${
+      ev.deltaX / 10
+    }deg)`;
+  }
 
-	onEnd(ev: any, card: HTMLElement, index: number) {
-		if (ev.deltaX > 150) {
-			this.swipeRight(card, index);
-		} else if (ev.deltaX < -150) {
-			this.swipeLeft(card, index);
-		} else {
-			card.style.transform = ''; // Reset si le swipe est insuffisant
-		}
-	}
+  onEnd(ev: any, card: HTMLElement, index: number) {
+    if (ev.deltaX > 150) {
+      this.swipeRight(card, index);
+    } else if (ev.deltaX < -150) {
+      this.swipeLeft(card, index);
+    } else {
+      card.style.transform = ''; // Reset si le swipe est insuffisant
+    }
+  }
 
-	swipeRight(card: HTMLElement, index: number) {
-		card.style.transform = 'translate(1000px, 0) rotate(20deg)';
-		card.style.opacity = '0';
-		setTimeout(() => this.removeCard(index), 300);
-		console.log('Swiped Right!', this.videos[index]);
-	}
+  swipeRight(card: HTMLElement, index: number) {
+    card.style.transform = 'translate(1000px, 0) rotate(20deg)';
+    card.style.opacity = '0';
+    setTimeout(() => this.removeCard(index), 300);
+    console.log('Swiped Right!', this.videos[index]);
+  }
 
-	swipeLeft(card: HTMLElement, index: number) {
-		card.style.transform = 'translate(-1000px, 0) rotate(-20deg)';
-		card.style.opacity = '0';
-		setTimeout(() => this.removeCard(index), 300);
-		console.log('Swiped Left!', this.videos[index]);
-	}
+  swipeLeft(card: HTMLElement, index: number) {
+    card.style.transform = 'translate(-1000px, 0) rotate(-20deg)';
+    card.style.opacity = '0';
+    setTimeout(() => this.removeCard(index), 300);
+    console.log('Swiped Left!', this.videos[index]);
+  }
 
-	removeCard(index: number) {
-		this.videos.splice(index, 1);
-	}
+  removeCard(index: number) {
+    this.videos.splice(index, 1);
+  }
 
-	toggleLike(index: number) {
-		this.videos[index].isLiked = !this.videos[index].isLiked;
-	}
+  toggleLike(index: number) {
+    this.videos[index].isLiked = !this.videos[index].isLiked;
+  }
 
+  openModal(index: number) {
+    this.modals[index] = true;
+    this.cdr.detectChanges();
+  }
+
+  closeModal(index: number) {
+    this.modals[index] = false;
+    this.cdr.detectChanges();
+  }
 }
