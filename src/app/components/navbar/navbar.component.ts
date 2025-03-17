2159d7d122e834d5712baf699d0af4ca57927178
Creative Eye Studio <contact@creative-eye.fr>
2025-03-17 17:52:33 +0100

    Active Link Nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonicModule } from "@ionic/angular";
 import { addIcons } from 'ionicons';
-import { addCircleOutline, chatbox, chatboxEllipsesSharp, chatbubblesOutline, homeOutline, notificationsOutline, options, personCircleOutline, search } from 'ionicons/icons';
+import { addCircleOutline, chatboxEllipsesOutline, chatboxEllipsesSharp, chatbubblesOutline, homeOutline, homeSharp, optionsOutline, optionsSharp, personCircleOutline, search } from 'ionicons/icons';
 import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
-    imports: [IonicModule]
+    imports: [IonicModule, CommonModule]
 })
 export class NavbarComponent {
-  constructor(public global: GlobalService) {
-    addIcons({ homeOutline, search, options, chatboxEllipsesSharp, addCircleOutline, chatbubblesOutline, personCircleOutline })
+  constructor(public global: GlobalService, public router: Router) {
+    addIcons({ homeOutline, homeSharp, search, optionsOutline, optionsSharp, chatboxEllipsesOutline, chatboxEllipsesSharp, addCircleOutline, chatbubblesOutline, personCircleOutline })
+  }
+
+  isActiveRoute(route: string): boolean {
+    console.log(this.router.url);
+    
+    return this.router.url === route
   }
 }
