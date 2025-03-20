6e18191c7456ed4b6254f89905d55be697c1056e
Creative Eye Studio <contact@creative-eye.fr>
2025-03-20 18:29:24 +0100

    Create profile interests

--- src/app/pages/create-profile-interests/create-profile-interests.page.ts
+++ src/app/pages/create-profile-interests/create-profile-interests.page.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonItem, IonGrid, IonRow, IonCol, IonButton, IonCheckbox, IonInput, IonSelect, IonSelectOption, IonList } from '@ionic/angular/standalone';
+import { IonContent, IonItem, IonGrid, IonRow, IonCol, IonButton, IonCheckbox, IonInput, IonSelect, IonSelectOption, IonList, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-create-profile-interests',
   templateUrl: './create-profile-interests.page.html',
   styleUrls: ['./create-profile-interests.page.scss'],
   standalone: true,
-  imports: [IonList, IonInput, IonCheckbox, IonButton, IonCol, IonRow, IonGrid, IonItem, IonContent, IonSelect, IonSelectOption, CommonModule, FormsModule]
+  imports: [IonLabel, IonIcon, IonList, IonInput, IonCheckbox, IonButton, IonCol, IonRow, IonGrid, IonItem, IonContent, IonSelect, IonSelectOption, CommonModule, FormsModule]
 })
 export class CreateProfileInterestsPage {
 
-  constructor() { }
+  constructor() {
+    addIcons({addOutline})
+  }
 
 }
