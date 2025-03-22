26be34b4d7234f85388771c0a702ab5a01f27fba
Creative Eye Studio <contact@creative-eye.fr>
2025-03-22 09:32:36 +0100

    Student Tabs

--- src/app/pages/student/components/tab-1/tab-1.component.ts
+++ src/app/pages/student/components/tab-1/tab-1.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { IonGrid, IonRow, IonCol, IonImg } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-tab-1',
   templateUrl: './tab-1.component.html',
   styleUrls: ['./tab-1.component.scss'],
-  imports: [IonRow, IonCol, IonGrid]
+  imports: [IonImg, IonRow, IonCol, IonGrid]
 })
 export class Tab1Component {
 
--- src/app/pages/student/components/tab-3/tab-3.component.ts
+++ src/app/pages/student/components/tab-3/tab-3.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { IonGrid, IonRow, IonCol, IonImg } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-tab-3',
   templateUrl: './tab-3.component.html',
   styleUrls: ['./tab-3.component.scss'],
-  imports: [IonRow, IonCol, IonGrid]
+  imports: [IonImg, IonRow, IonCol, IonGrid]
 })
 export class Tab3Component {
 
