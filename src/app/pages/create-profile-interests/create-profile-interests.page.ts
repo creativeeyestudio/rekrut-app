7b855a6f5651e7572a3388d1cb9824b40ac03b9d
Creative Eye Studio <contact@creative-eye.fr>
2025-03-24 00:15:22 +0100

    Student Skills

--- src/app/pages/create-profile-interests/create-profile-interests.page.ts
+++ src/app/pages/create-profile-interests/create-profile-interests.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonItem, IonGrid, IonRow, IonCol, IonButton, IonCheckbox, IonSelect, IonSelectOption, IonList, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonItem, IonGrid, IonRow, IonCol, IonButton, IonCheckbox, IonSelect, IonSelectOption, IonList, IonListHeader } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { addOutline } from 'ionicons/icons';
 import { GlobalService } from 'src/app/services/global.service';
@@ -11,9 +11,13 @@ import { GlobalService } from 'src/app/services/global.service';
   templateUrl: './create-profile-interests.page.html',
   styleUrls: ['./create-profile-interests.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonList, IonCheckbox, IonButton, IonCol, IonRow, IonGrid, IonItem, IonContent, IonSelect, IonSelectOption, CommonModule, FormsModule]
+  imports: [IonList, IonCheckbox, IonButton, IonCol, IonRow, IonGrid, IonItem, IonContent, IonSelect, IonSelectOption, CommonModule, FormsModule]
 })
 export class CreateProfileInterestsPage {
+
+  softSkills: string[] = ['Autonome', 'Esprit d\'équipe', 'Innovant', 'Amélioratif', 'Dirigeant', 'Leadership'];
+  workStyleSkills: string[] = ['Télétravail', 'Présentiel', 'Structuré', 'Flexible', 'Travail intense', 'Rythme équilibré'];
+  valuesSkills: string[] = ['Inclusion', 'Solidarité', 'Égalité', 'Bienveillance', 'Justice', 'Partage', 'Persévérance', 'Ambition', 'Créativité', 'Innovation', 'Éthique', 'Passion', 'Stabilité', 'Audace', 'Exigence', 'Excellence', 'Curiosité', 'Optimisme', 'Proactivité', 'Intégrité', 'Discrétion', 'Polyvalence', 'Discipline', 'Vision', 'Indépendance', 'Loyauté', 'Réalisme', 'Esprit critique', 'Esprit entrepreneurial'];
 
   constructor(public global: GlobalService) {
     addIcons({addOutline});
