263da12baa431cd10bc5b2d8dd51d4360a154ea0
Creative Eye Studio <contact@creative-eye.fr>
2025-03-25 20:28:21 +0100

    Register Page Margin Top

--- src/app/pages/tutorial/tutorial.page.ts
+++ src/app/pages/tutorial/tutorial.page.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonImg } from '@ionic/angular/standalone';
@@ -9,52 +15,52 @@ import { GlobalService } from 'src/app/services/global.service';
   templateUrl: './tutorial.page.html',
   styleUrls: ['./tutorial.page.scss'],
   standalone: true,
-  imports: [IonImg, IonButton, IonContent, CommonModule, FormsModule]
+  imports: [IonImg, IonButton, IonContent, CommonModule, FormsModule],
 })
-export class TutorialPage implements AfterViewInit  {
+export class TutorialPage implements AfterViewInit {
   @ViewChildren('tutoElement') tutoElements!: QueryList<ElementRef>;
 
   tutoPos: number = 0;
-  
+
   tutoBlocks = [
     {
-      id: 0, 
+      id: 0,
       visible: true,
       title: 'Crée et complète ton profil',
       desc: 'Complète ton profil et ajoute ton CV pour attirer l’attention des recruteurs.',
       image: 'tuto-1.png',
     },
     {
-      id: 1, 
+      id: 1,
       visible: false,
       title: 'Postule aux offres qui te correspondent',
       desc: 'Après avoir cliqué sur "Uploader la vidéo skill", tu devras choisir un secteur et un niveau de difficulté. Une question choisie par le recruteur pourra t\'être ensuite posée.',
       image: 'tuto-2.png',
     },
     {
-      id: 2, 
+      id: 2,
       visible: false,
       title: 'Réponds à la question en vidéo',
-      desc: 'Ferme l’application, enregistre ta vidéo (moins de une minute) en répondant à la question, puis upload-la sur l\'application.',
+      desc: "Ferme l’application, enregistre ta vidéo (moins de une minute) en répondant à la question, puis upload-la sur l'application.",
       image: 'tuto-3.png',
     },
     {
-      id: 3, 
+      id: 3,
       visible: false,
-      title: 'C\'est tout bon',
-      desc: 'Attends que les recruteurs te contactent et n\'oublie pas d\'activer les notifications pour être au courant des derniers matchs.',
+      title: "C'est tout bon",
+      desc: "Attends que les recruteurs te contactent et n'oublie pas d'activer les notifications pour être au courant des derniers matchs.",
       image: 'tuto-4.png',
     },
     {
-      id: 3, 
+      id: 3,
       visible: false,
-      title: 'L\'application est encore en développement',
-      desc: 'Nous sommes en train d\'améliorer l\'application. N\'hésitez pas à nous faire vos retours sur votre expérience.',
+      title: "L'application est encore en développement",
+      desc: "Nous sommes en train d'améliorer l'application. N'hésitez pas à nous faire vos retours sur votre expérience.",
       image: 'tuto-5.png',
     },
-  ]
+  ];
 
-  constructor(public global: GlobalService) { }
+  constructor(public global: GlobalService) {}
 
   goToTheNext() {
     if (this.tutoPos < this.tutoBlocks.length - 1) {
@@ -68,11 +74,10 @@ export class TutorialPage implements AfterViewInit  {
 
   goToHome() {
     this.global.isNavHidden = false;
-    this.global.navigate('fr')
+    this.global.navigate('fr');
   }
 
   ngAfterViewInit(): void {
-      this.global.isNavHidden = true;
+    this.global.isNavHidden = true;
   }
-  
 }
