35a2fd195f99615fa781400d71c57623ccbaaee0
Creative Eye Studio <contact@creative-eye.fr>
2025-03-26 18:42:40 +0100

    Ionic corrections

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonIcon, IonInput, IonInputPasswordToggle, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonImg, IonIcon, IonInput, IonInputPasswordToggle, IonButton } from '@ionic/angular/standalone';
 import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { GlobalService } from 'src/app/services/global.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonIcon, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, IonInputPasswordToggle, CommonModule, FormsModule]
+  imports: [IonButton, IonInput, IonIcon, IonImg, IonContent, IonInputPasswordToggle, CommonModule, FormsModule]
 })
 export class LoginPage  {
 
--- src/app/pages/skills-desc/skills-desc.page.ts
+++ src/app/pages/skills-desc/skills-desc.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonSelect, IonSelectOption, IonItem, IonIcon, IonList, IonInput, IonTextarea, IonCheckbox, IonButton, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonSelect, IonSelectOption, IonItem, IonIcon, IonList, IonInput, IonCheckbox, IonButton, IonImg } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { chevronBackOutline } from 'ionicons/icons';
 import { GlobalService } from 'src/app/services/global.service';
@@ -11,7 +11,7 @@ import { GlobalService } from 'src/app/services/global.service';
   templateUrl: './skills-desc.page.html',
   styleUrls: ['./skills-desc.page.scss'],
   standalone: true,
-  imports: [IonImg, IonButton, IonCheckbox, IonTextarea, IonList, IonIcon, IonItem, IonContent, IonSelect, IonSelectOption, IonInput, CommonModule, FormsModule]
+  imports: [IonImg, IonButton, IonCheckbox, IonList, IonIcon, IonItem, IonContent, IonSelect, IonSelectOption, IonInput, CommonModule, FormsModule]
 })
 export class SkillsDescPage {
 
