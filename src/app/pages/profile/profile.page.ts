5474debb553885616b2e1380c4fb4e2d25e8b8b6
Creative Eye Studio <contact@creative-eye.fr>
2025-03-29 09:40:27 +0100

    Profile tabs

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,46 +1,93 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ViewChild,
+} from '@angular/core';
+import Swiper from 'swiper';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { GlobalService } from 'src/app/services/global.service';
 
 import { addIcons } from 'ionicons';
-import { addCircleOutline, bookmarkOutline, cameraOutline, chevronBackOutline, heartOutline, linkOutline, playCircleOutline, logoFacebook, logoWhatsapp, settingsOutline, shareSocialOutline, lockClosedOutline } from 'ionicons/icons';
-import { Tab1Component } from "./components/tab-1/tab-1.component";
+import {
+  addCircleOutline,
+  bookmarkOutline,
+  cameraOutline,
+  chevronBackOutline,
+  heartOutline,
+  linkOutline,
+  playCircleOutline,
+  logoFacebook,
+  logoWhatsapp,
+  settingsOutline,
+  shareSocialOutline,
+  lockClosedOutline,
+} from 'ionicons/icons';
+import { Tab1Component } from './components/tab-1/tab-1.component';
 import { Tab2Component } from './components/tab-2/tab-2.component';
-import { Tab3Component } from "./components/tab-3/tab-3.component";
-import { Tab4Component } from "./components/tab-4/tab-4.component";
+import { Tab3Component } from './components/tab-3/tab-3.component';
+import { Tab4Component } from './components/tab-4/tab-4.component';
+
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.page.html',
-    styleUrls: ['./profile.page.scss'],
-    imports: [IonicModule, CommonModule, FormsModule, Tab1Component, Tab2Component, Tab3Component, Tab4Component]
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab1Component,
+    Tab2Component,
+    Tab3Component,
+    Tab4Component,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ProfilePage  {
-
+export class ProfilePage implements AfterViewInit {
+  @ViewChild('swiperRef', { static: false }) swiperRef: any;
+  swiper!: Swiper;
   isShareLinksVisible: boolean = false;
 
   constructor(public global: GlobalService) {
-    addIcons({ settingsOutline, chevronBackOutline, addCircleOutline, cameraOutline, playCircleOutline, bookmarkOutline, heartOutline, logoFacebook , logoWhatsapp, shareSocialOutline, linkOutline, lockClosedOutline });
+    addIcons({
+      settingsOutline,
+      chevronBackOutline,
+      addCircleOutline,
+      cameraOutline,
+      playCircleOutline,
+      bookmarkOutline,
+      heartOutline,
+      logoFacebook,
+      logoWhatsapp,
+      shareSocialOutline,
+      linkOutline,
+      lockClosedOutline,
+    });
   }
 
-  changeTab(tabId: string) {
-    // Retrait des onglets actifs
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach((tab) => {
-      tab.classList.remove('active')
-    })
+  ngAfterViewInit() {
+    // Attendre un petit délai pour être sûr que Swiper est bien attaché
+    setTimeout(() => {
+      if (this.swiperRef?.nativeElement?.swiper) {
+        this.swiper = this.swiperRef.nativeElement.swiper;
+      } else {
+        console.error("Swiper n'a pas pu être initialisé.");
+      }
+    }, 100);
+  }
 
-    // Affichage de l'onglet sélectionné
-    const tab = document.getElementById(tabId);
-    tab?.classList.add('active');
-
-    console.log("Called");
+  goToSlide(index: number) {
+    if (this.swiper) {
+      this.swiper.slideTo(index);
+    }
   }
 
   toggleShareLinksVisible() {
-    this.isShareLinksVisible = !this.isShareLinksVisible
+    this.isShareLinksVisible = !this.isShareLinksVisible;
   }
-
 }
