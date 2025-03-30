9af436e0aaa0cce74ddcbac63ba7a60935799513
Creative Eye Studio <contact@creative-eye.fr>
2025-03-30 22:11:06 +0200

    Student page

--- src/app/pages/student/student.page.ts
+++ src/app/pages/student/student.page.ts
@@ -17,8 +17,7 @@ import { addIcons } from 'ionicons';
 import {
   bookmarkOutline,
   heartOutline,
-  lockClosedOutline,
-} from 'ionicons/icons';
+  lockClosedOutline, playCircleOutline } from 'ionicons/icons';
 import { Tab1Component } from './components/tab-1/tab-1.component';
 import { Tab2Component } from './components/tab-2/tab-2.component';
 import { Tab3Component } from './components/tab-3/tab-3.component';
@@ -60,7 +59,7 @@ export class StudentPage implements AfterViewInit{
   activeSlideIndex: number = 0;
   
   constructor() {
-    addIcons({ lockClosedOutline, bookmarkOutline, heartOutline });
+    addIcons({playCircleOutline,lockClosedOutline,bookmarkOutline,heartOutline});
   }
 
   ngAfterViewInit() {
