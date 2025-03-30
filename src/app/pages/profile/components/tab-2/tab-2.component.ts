25cbe8b9a6ba64605a11e5b65c10a57d201d2787
Creative Eye Studio <contact@creative-eye.fr>
2025-03-30 22:49:17 +0200

    Add Video Button

--- src/app/pages/profile/components/tab-2/tab-2.component.ts
+++ src/app/pages/profile/components/tab-2/tab-2.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonGrid, IonLabel, IonIcon, IonCol, IonRow } from "@ionic/angular/standalone";
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-tab-2',
   templateUrl: './tab-2.component.html',
   styleUrls: ['./tab-2.component.scss'],
-  imports: [IonRow, IonCol, IonGrid, CommonModule]
+  imports: [IonRow, IonIcon, IonCol, IonGrid, CommonModule]
 })
 export class Tab2Component {
 
-  constructor() { }
+  constructor(public global: GlobalService) { }
 
 }
